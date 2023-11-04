c1e96a14d98dd7b69af74c072de29035b269ed92
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2023-11-04 15:15:16 +0800

    Fix typos and update file paths in clone.sh

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/gongbaodd/gongbaodd.github.io.git ./tmp/blog
 rm -rf ./src/content/blog
-cp -r ./tmp/blog/_doc ./src/content/blog
+cp -r ./tmp/blog/_docs ./src/content/blog
