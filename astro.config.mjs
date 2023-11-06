a60d60e8a8dd4de9a520b257877b36687b4d9657
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2023-11-06 11:18:17 +0800

    Add Tailwind CSS and update Demo component.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://growgen.xyz',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), tailwind()],
   title: "宫不上的博客"
 });
--- src/components/demo.tsx
+++ src/components/demo.tsx
@@ -1,5 +1,9 @@
 const Demo = () => {
-    return "hello world"
+    return (
+        <h1 class="text-3xl font-bold underline">
+            hello world
+        </h1>
+    )
 }
 
 export default Demo
