332fba9c9778f6587d906edc445120935a85b9ab
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2023-11-08 15:14:03 +0800

    Update site title and description in consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'GrowGen';
-export const SITE_DESCRIPTION = `Let's grow together!`;
+export const SITE_TITLE = 'GrowGen | 给我整';
+export const SITE_DESCRIPTION = `生命在于折腾`;
