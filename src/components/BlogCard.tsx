8fe8e9c04f49991e7b6a739704ef39e25f552bf5
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2023-11-08 16:12:43 +0800

    Add dayjs library and format blog post dates.

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -2,6 +2,7 @@ import type { FC } from "react"
 import React from "react"
 import { Card, CardContent, CardTitle, CardDescription, CardHeader, CardFooter } from "@/components/ui/card"
 import { Button } from "./ui/button"
+import dayjs from "dayjs"
 
 interface Props {
     title: string
@@ -13,7 +14,7 @@ const BlogCard: FC<Props> = ({ title, date }) => {
         <Card>
             <CardHeader>
                 <CardTitle>{title}</CardTitle>
-                <CardDescription>{date}</CardDescription>
+                <CardDescription>{dayjs(date).format("MMMM D, YYYY")}</CardDescription>
             </CardHeader>
             <CardContent></CardContent>
             <CardFooter className="flex-row-reverse">
