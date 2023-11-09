03710cbc6294bb201eb5842c5ca99b327b86209d
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2023-11-09 15:23:43 +0800

    Add category and link to BlogCard, update header style, and remove commented out code

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,27 +1,31 @@
-import type { FC } from "react"
-import React from "react"
+import React, { type FC } from "react"
 import { Card, CardContent, CardTitle, CardDescription, CardHeader, CardFooter } from "@/components/ui/card"
 import { buttonVariants } from "./ui/button"
+import { badgeVariants } from "./ui/badge"
 import dayjs from "dayjs"
+import { cn } from "@/lib/utils"
 
 interface Props {
     title: string
     date: Date
     excerpt: string
+    category: string
+    link: string
 }
 
-const BlogCard: FC<Props> = ({ title, date, excerpt }) => {
+const BlogCard: FC<Props> = ({ title, date, excerpt, category, link }) => {
     return (
         <Card>
-            <CardHeader>
+            <CardHeader className="relative">
                 <CardTitle>{title}</CardTitle>
+                <a className={cn(badgeVariants({ variant: "secondary" }), "absolute top-0 right-1")}>{category}</a>
                 <CardDescription>{dayjs(date).format("MMMM D, YYYY")}</CardDescription>
             </CardHeader>
             <CardContent>
                 <p>{excerpt}</p>
             </CardContent>
             <CardFooter className="flex-row-reverse">
-                <a className={buttonVariants({ variant: "outline" })}>Read More</a>
+                <a className={buttonVariants({ variant: "outline" })} href={link}>Read More</a>
             </CardFooter>
         </Card>
     )
