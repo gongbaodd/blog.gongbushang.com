87adacaa08cc9f461a8f891db948cfc2599633d5
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2023-11-10 15:26:12 +0800

    Add math support and grid layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,26 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import react from "@astrojs/react"
+import tailwind from "@astrojs/tailwind"
 
-import tailwind from "@astrojs/tailwind";
+import remarkMath from "remark-math"
+import rehypeKatex from "rehype-katex"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://growgen.xyz',
+  site: "https://growgen.xyz",
   integrations: [
-    mdx(), sitemap(), react(),
+    mdx(),
+    sitemap(),
+    react(),
     tailwind({
       applyBaseStyles: false,
     }),
   ],
-  title: "宫不上的博客"
-});
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+  title: "宫不上的博客",
+})
