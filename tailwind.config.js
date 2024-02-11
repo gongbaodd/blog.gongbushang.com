954cb3b83f0739f7a4cd67807ab4d370df890afd
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2024-02-11 14:58:11 +0700

    Add global.css import and update Tailwind content configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,9 @@
 const config = require("./packages/shadcn/tailwind.config")
 
-module.exports = config
+module.exports = {
+    ...config,
+    content: [
+        "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+        "./packages/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"
+    ]
+}
