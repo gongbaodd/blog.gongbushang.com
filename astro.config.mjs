d218e8c9fc59a9762d7e167d4914dc9cb7bb9b3c
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2024-02-11 15:36:09 +0700

    Update package.json and astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,31 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
-import config from "./packages/config"
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import rehypeExternalLinks from "rehype-external-links";
 
-export default config
+export default defineConfig({
+  site: "https://growgen.xyz",
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [
+      [rehypeKatex, { strict: false }],
+      [
+        rehypeExternalLinks,
+        { target: "_blank", rel: ["noopener", "noreferrer", "nofollow"] },
+      ],
+    ],
+  },
+});
