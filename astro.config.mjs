3e433f585da47edcfaec1354f558bf6ee9519b6b
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2024-02-11 10:29:10 +0700

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,4 @@
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
-import react from "@astrojs/react"
-import tailwind from "@astrojs/tailwind"
-
-import remarkMath from "remark-math"
-import remarkLicense from "remark-license"
-import rehypeKatex from "rehype-katex"
-import rehypeExternalLinks from "rehype-external-links"
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://growgen.xyz",
-  integrations: [
-    mdx(),
-    sitemap(),
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
-  markdown: {
-    remarkPlugins: [remarkMath, remarkLicense],
-    rehypePlugins: [
-      rehypeKatex,
-      [
-        rehypeExternalLinks,
-        { target: "_blank", rel: ["noopener", "noreferrer", "nofollow"] },
-      ],
-    ],
-  },
-  title: "宫不上的博客",
-})
+export default defineConfig({});
--- scripts/clone.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/gongbaodd/gongbaodd.github.io.git ./tmp/blog
-rm -rf ./src/content/blog
-cp -r ./tmp/blog/_docs ./src/content/blog
