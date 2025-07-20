fdc71d69444c2fabf20e063748048be692416d04
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2025-07-20 17:11:15 +0300

    fix clone.sh

--- packages/scripts/clone.sh
+++ packages/scripts/clone.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/gongbaodd/gongbaodd.github.io.git ./tmp/blog
-rm -rf ./src/content/blog
-cp -r ./tmp/blog/_docs ./src/content/blog
+rm -rf ./src/content/_docs
+cp -r ./tmp/blog/_docs ./src/content/_docs
