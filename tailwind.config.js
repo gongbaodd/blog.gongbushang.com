75d911a9c9e929405ce112acaad8b593194a34e4
gongbaodd@ProArt <gongbaodd@qq.com>
2025-08-03 15:29:03 +0300

    update shadcn css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,6 @@ module.exports = {
         "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
         "./packages/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
          "!./packages/**/node_modules/**/*"
-    ]
+    ],
+    plugins: [require('@tailwindcss/typography')]
 }
