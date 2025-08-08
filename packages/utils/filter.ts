78b1326663a57d88e1c971deb5522ec018c4d2a5
gongbaodd@ProArt <gongbaodd@qq.com>
2025-08-08 17:36:57 +0300

    separate category/tag/series logic

--- packages/utils/filter.ts
+++ packages/utils/filter.ts
@@ -35,23 +35,24 @@ export const getAllFilterEntries = async () => {
   }
 }
 
-export const getFilteredPage = async () => {
+let categoryPostMap = new Map<string, Set<T_POST>>()
+function initCategoryPostMap(posts: Set<T_POST> | T_POST[]) {
+  const init = memoize(() => {
+    categoryPostMap = createPostMap(posts, (p) => [p.data.category])
+    return categoryPostMap
+  }, { getCacheKey: () => Array.from(posts).length })
+  return init()
+}
+
+export const getFilterByCategoryPage = async () => {
   const posts = await getCollection("blog")
-
-  const getMemorizedCategoryResult = memoize(() => getStaticPathsByFilter(posts, (p) => [p.data.category]), { getCacheKey: () => posts.length });
-  const categoryResult = getMemorizedCategoryResult();
-
-  const getMemorizedTagResult = memoize(() => getStaticPathsByFilter(posts, (p) => (p.data.tag ?? []).map((t: string) => `${FILTER_ENTRY.TAG}/${t.toLowerCase()}`)), { getCacheKey: () => posts.length });
-  const tagResult = getMemorizedTagResult();
-
-  const getMemorizedSeriesResult = memoize(() => getStaticPathsByFilter(posts, (p) =>
-    p.data.series?.slug ? [`${FILTER_ENTRY.SERIES}/${p.data.series.slug}`] : []
-  ), { getCacheKey: () => posts.length });
-  const seriesResult = getMemorizedSeriesResult();
-
-  const [yearResult, yearMonthResult, yearMonthDayResult] = addDateFilter(
-    () => [FILTER_ENTRY.ALL],
-  )
+  const categoryPostMap = initCategoryPostMap(posts)
+  const categoryResult = Array.from(categoryPostMap, ([filter, postsSet]) => ({
+    params: {
+      filter,
+    },
+    props: { posts: sortPostsByDate(Array.from(postsSet)) },
+  }))
 
   return [
     {
@@ -63,104 +64,80 @@ export const getFilteredPage = async () => {
       },
     },
     ...categoryResult,
-    ...tagResult,
-    ...seriesResult,
-    ...yearResult,
-    ...yearMonthResult,
-    ...yearMonthDayResult,
   ]
+}
 
-  function getStaticPathsByFilter(
-    posts: Set<T_POST> | T_POST[],
-    filterFn: (p: T_POST) => string[]
-  ) {
-    const items = new Map<string, Set<T_POST>>()
+export const isValidCategoryFilter = async (filter: string) => {
+  if (filter === FILTER_ENTRY.ALL) return true
 
-    for (const post of posts) {
-      const filters = filterFn(post)
-      filters.forEach((filter) => {
-        if (!items.has(filter)) {
-          items.set(filter, new Set<T_POST>())
-        }
-        items.get(filter)?.add(post)
-      })
-    }
+  const posts = await getCollection("blog")
+  const categoryPostMap = initCategoryPostMap(posts)
+  const categoryEntries = Array.from(categoryPostMap, ([filter]) => filter)
 
-    const result = []
-    for (const [filter, posts] of items) {
-      result.push({
-        params: {
-          filter,
-        },
-        props: {
-          posts: sortPostsByDate(Array.from(posts)),
-        },
-      })
-    }
+  return categoryEntries.some(v => filter === v)
+}
 
-    return result
-  }
+let tagPostMap = new Map<string, Set<T_POST>>()
+function initTagPostMap(posts: Set<T_POST> | T_POST[]) {
+  const init = memoize(() => {
+    tagPostMap = createPostMap(posts, (p) => (p.data.tag ?? []).map((t: string) => t.toLowerCase()))
+    return tagPostMap
+  }, { getCacheKey: () => Array.from(posts).length })
+  return init()
+}
 
-  function sortPostsByDate(posts: T_POST[]) {
-    return posts.toSorted((p1, p2) => {
-      const d1 = new Date(date(p1))
-      const d2 = new Date(date(p2))
-      return d1 > d2 ? -1 : 1
-    })
-  }
+export const getFilterByTagPage = async () => {
+  const posts = await getCollection("blog")
+  const tagPostMap = initTagPostMap(posts)
+  const tagResult = Array.from(tagPostMap, ([filter, postsSet]) => ({
+    params: {
+      filter,
+    },
+    props: { posts: sortPostsByDate(Array.from(postsSet)) },
+  }))
 
-  function addFilter(
-    preItems: ReturnType<typeof getStaticPathsByFilter>,
-    filterFn: (p: T_POST) => string[]
-  ) {
-    const results = []
-    for (const preItem of preItems) {
-      const { posts } = preItem.props
-      const result = getStaticPathsByFilter(posts, filterFn)
+  return tagResult
+}
 
-      results.push(...result)
-    }
+export const isValidTagFilter = async (filter: string) => {
+  const posts = await getCollection("blog")
+  const tagPostMap = initTagPostMap(posts)
+  const tagEntries = Array.from(tagPostMap, ([filter]) => filter)
 
-    return results
-  }
+  return tagEntries.some(v => filter === v)
+}
 
-  function addDateFilter(
-    filterFn: (p: T_POST) => string[],
-    result?: ReturnType<typeof getStaticPathsByFilter>
-  ) {
-    const yearResult = result
-      ? addFilter(result, (p) => {
-        const filters = filterFn(p)
-        const _date = date(p)
-        const year = _date.getFullYear().toString()
-        return filters.map((filter) => `${filter}/${year}`)
-      })
-      : getStaticPathsByFilter(posts, (p) => {
-        const filters = filterFn(p)
-        const _date = date(p)
-        const year = _date.getFullYear().toString()
-        return filters.map((filter) => `${filter}/${year}`)
-      })
 
-    const yearMonthResult = addFilter(yearResult, (p) => {
-      const filters = filterFn(p)
-      const _date = date(p)
-      const year = _date.getFullYear().toString()
-      const month = (_date.getMonth() + 1).toString().padStart(2, "0")
-      return filters.map((filter) => `${filter}/${year}/${month}`)
-    })
+let seriesPostMap = new Map<string, Set<T_POST>>()
+function initSeriesPostMap(posts: Set<T_POST> | T_POST[]) {
+  const init = memoize(() => {
+    seriesPostMap = createPostMap(posts, (p) =>
+    p.data.series?.slug ? [p.data.series.slug] : []
+  )
+    return seriesPostMap
+  }, { getCacheKey: () => Array.from(posts).length })
+  return init()
+}
 
-    const yearMonthDayResult = addFilter(yearMonthResult, (p) => {
-      const filters = filterFn(p)
-      const _date = date(p)
-      const year = _date.getFullYear().toString()
-      const month = (_date.getMonth() + 1).toString().padStart(2, "0")
-      const day = _date.getDate().toString().padStart(2, "0")
-      return filters.map((filter) => `${filter}/${year}/${month}/${day}`)
-    })
+export const getFilterBySeriesPage = async () => {
+  const posts = await getCollection("blog")
+  const seriesPostMap = initSeriesPostMap(posts)
+  const seriesResult = Array.from(seriesPostMap, ([filter, postsSet]) => ({
+    params: {
+      filter,
+    },
+    props: { posts: sortPostsByDate(Array.from(postsSet)) },
+  }))
 
-    return [yearResult, yearMonthResult, yearMonthDayResult]
-  }
+  return seriesResult
+}
+
+export const isValidSeriesFilter = async (filter: string) => {
+  const posts = await getCollection("blog")
+  const tagPostMap = initSeriesPostMap(posts)
+  const tagEntries = Array.from(tagPostMap, ([filter]) => filter)
+
+  return tagEntries.some(v => filter === v)
 }
 
 export async function mapServerPostToClient(posts: T_POST[]) {
@@ -180,4 +157,29 @@ export async function isValidFilter(filter: string) {
   const validFiltes = await getAllFilterEntries();
   const parts = filter.split("/").filter(Boolean) ?? [];
   return validFiltes.some((f) => f === parts[0]);
+}
+
+
+function createPostMap(posts: Set<T_POST> | T_POST[], filterFn: (p: T_POST) => string[]) {
+  const items = new Map<string, Set<T_POST>>()
+
+  for (const post of posts) {
+    const filters = filterFn(post)
+    filters.forEach((filter) => {
+      if (!items.has(filter)) {
+        items.set(filter, new Set<T_POST>())
+      }
+      items.get(filter)?.add(post)
+    })
+  }
+
+  return items
+}
+
+function sortPostsByDate(posts: T_POST[]) {
+  return posts.toSorted((p1, p2) => {
+    const d1 = new Date(date(p1))
+    const d2 = new Date(date(p2))
+    return d1 > d2 ? -1 : 1
+  })
 }
