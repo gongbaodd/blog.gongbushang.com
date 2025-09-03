4fc2de1ee1be897996652f8667adb05b21c8a411
gongbaodd@ProArt <gongbaodd@qq.com>
2025-09-03 14:51:53 +0300

    page transitions

--- packages/card/PostCard.tsx
+++ packages/card/PostCard.tsx
@@ -6,6 +6,7 @@ import { Calendar } from "lucide-react";
 import { useEffect, useState } from "react";
 import type { TClientPost } from "../utils/post";
 import classes from "./PostCard.module.css"
+import CustomMantineProvider from "@/src/stores/CustomMantineProvider";
 
 export interface IPost extends TClientPost {
 }
@@ -57,109 +58,112 @@ export function PostCard({ post, hideExcerpt }: ICardProp) {
   }
 
   return (
-    <Stack justify={"center"} align={"center"}>
-      <Box maw={360}>
-        <Anchor underline="never" href={post.href}>
-          <Card
-            key={post.id}
-            shadow="sm"
-            padding="lg"
-            radius="lg"
-            withBorder
-            className={className}
-            style={{
-              backgroundColor: post.data.bgColor,
-              "--underline-color": `var(${post.data.titleColor})`,
-              "--cover-opacity": coverOpacity,
-              "--cover-image": `url(${coverImage})`,
-              "--cover-trace": tracedCover
-            }}
-          >
-            <Flex direction={"column"} justify={"space-between"} flex={1} className={classes.content}>
-              <Flex justify={"space-between"} align={"center"}>
-                <Badge
-                  color="gray"
-                  variant="default"
-                  size="sm"
-                  className={classes.category}
-                >
-                  <Group gap={6}>
-                    <Calendar size={12} />
-                    <Text size="xs">{dayjs(post.date).format("YYYY-MM-DD")}</Text>
-                  </Group>
-                </Badge>
-                <Avatar
-                  color="gray"
-                  variant="default"
-                  size="sm"
-                  className={classes.category}
-                  p={0}
-                >
-                  <ViewCount path={post.href} />
-                </Avatar>
-              </Flex>
-
-
-              <Title className={classes.title}>
-                <span>{title}</span>
-              </Title>
-
-              <Flex gap="xs" justify={"space-between"} align={"end"}>
-                <Group flex={1}>
+    <CustomMantineProvider>
+      <Stack justify={"center"} align={"center"}>
+        <Box maw={360}>
+          <Anchor underline="never" href={post.href}>
+            <Card
+              key={post.id}
+              shadow="sm"
+              padding="lg"
+              radius="lg"
+              withBorder
+              className={className}
+              style={{
+                backgroundColor: post.data.bgColor,
+                "--underline-color": `var(${post.data.titleColor})`,
+                "--cover-opacity": coverOpacity,
+                "--cover-image": `url(${coverImage})`,
+                "--cover-trace": tracedCover,
+                "--transition-name": "p-" + post.id.replaceAll("/", "-"),
+              }}
+            >
+              <Flex direction={"column"} justify={"space-between"} flex={1} className={classes.content}>
+                <Flex justify={"space-between"} align={"center"}>
                   <Badge
                     color="gray"
                     variant="default"
                     size="sm"
                     className={classes.category}
                   >
-                    {post.data.category}
+                    <Group gap={6}>
+                      <Calendar size={12} />
+                      <Text size="xs">{dayjs(post.date).format("YYYY-MM-DD")}</Text>
+                    </Group>
                   </Badge>
-                </Group>
+                  <Avatar
+                    color="gray"
+                    variant="default"
+                    size="sm"
+                    className={classes.category}
+                    p={0}
+                  >
+                    <ViewCount path={post.href} />
+                  </Avatar>
+                </Flex>
 
-                <Group gap="xs" flex={0} miw="5em">
-                  {post.data.series && (
+
+                <Title className={classes.title}>
+                  <span>{title}</span>
+                </Title>
+
+                <Flex gap="xs" justify={"space-between"} align={"end"}>
+                  <Group flex={1}>
                     <Badge
-                      key={post.data.series.slug}
                       color="gray"
                       variant="default"
-                      size="xs"
+                      size="sm"
                       className={classes.category}
                     >
-                      {post.data.series.name ?? post.data.series.slug}
+                      {post.data.category}
                     </Badge>
-                  )}
+                  </Group>
 
-                  {post.data.tag?.map((tag: string) => (
-                    <Badge
-                      key={tag}
-                      color="gray"
-                      variant="default"
-                      size="xs"
-                      className={classes.category}
-                    >
-                      #{tag}
-                    </Badge>
-                  ))}
-                </Group>
+                  <Group gap="xs" flex={0} miw="5em">
+                    {post.data.series && (
+                      <Badge
+                        key={post.data.series.slug}
+                        color="gray"
+                        variant="default"
+                        size="xs"
+                        className={classes.category}
+                      >
+                        {post.data.series.name ?? post.data.series.slug}
+                      </Badge>
+                    )}
+
+                    {post.data.tag?.map((tag: string) => (
+                      <Badge
+                        key={tag}
+                        color="gray"
+                        variant="default"
+                        size="xs"
+                        className={classes.category}
+                      >
+                        #{tag}
+                      </Badge>
+                    ))}
+                  </Group>
+                </Flex>
               </Flex>
+            </Card>
+          </Anchor>
+          {!hideExcerpt && (
+            <Flex pl={5} pr={10} pt={5}>
+              <Avatar
+                size="xs"
+                variant="transparent"
+                style={{ transform: "rotateZ(180deg)" }}
+              >
+                <IconQuoteFilled />
+              </Avatar>
+              <Text size="sm" lineClamp={2} className={classes.excerpt}>
+                {post.excerpt}
+              </Text>
             </Flex>
-          </Card>
-        </Anchor>
-        {!hideExcerpt && (
-          <Flex pl={5} pr={10} pt={5}>
-            <Avatar
-              size="xs"
-              variant="transparent"
-              style={{ transform: "rotateZ(180deg)" }}
-            >
-              <IconQuoteFilled />
-            </Avatar>
-            <Text size="sm" lineClamp={2} className={classes.excerpt}>
-              {post.excerpt}
-            </Text>
-          </Flex>
-        )}
-      </Box>
-    </Stack>
+          )}
+        </Box>
+      </Stack>
+    </CustomMantineProvider>
   );
 }
