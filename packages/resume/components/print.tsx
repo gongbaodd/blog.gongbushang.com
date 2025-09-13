26dd2cf8f63fb09efa4691f590c523c152324c4b
gongbaodd@ProArt <gongbaodd@qq.com>
2025-09-13 10:15:54 +0300

    try if can test the print function

--- packages/resume/components/print.tsx
+++ packages/resume/components/print.tsx
@@ -1,13 +1,29 @@
 import CustomMantineProvider from "@/src/stores/CustomMantineProvider";
 import { Affix, Button } from "@mantine/core";
-import { Printer } from "lucide-react";
+import { Printer, Download } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function PrintButton() {
+    const [supportPrint ,setSupportPrint] = useState(globalThis.print !== undefined)
+
+    // useEffect(() => {
+    //     setSupportPrint(globalThis.print !== undefined)
+    // }, [])
+
     return <CustomMantineProvider>
-        <Affix position={{  bottom: 20, right: 20 }}>
-            <Button variant="round" onClick={() => print()}>
-                <Printer size={26} />
-            </Button>
-        </Affix>
+        {supportPrint ? (
+            <Affix position={{ bottom: 20, right: 20 }}>
+                <Button variant="round" onClick={() => print()}>
+                    <Printer size={26} />
+                </Button>
+            </Affix>
+        ) : (
+            <Affix position={{ bottom: 20, right: 20 }}>
+                <Button variant="round" onClick={() => print()}>
+                    <Download size={26} />
+                </Button>
+            </Affix>
+        )}
+
     </CustomMantineProvider>
 }
