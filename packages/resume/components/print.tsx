67ff6584ff99037b5565f58de9eeb6e92082907a
gongbaodd@ProArt <gongbaodd@qq.com>
2025-09-13 10:55:31 +0300

    add download PDF

--- packages/resume/components/print.tsx
+++ packages/resume/components/print.tsx
@@ -1,14 +1,22 @@
 import CustomMantineProvider from "@/src/stores/CustomMantineProvider";
-import { Affix, Button } from "@mantine/core";
-import { Printer, Download } from "lucide-react";
+import { Affix, Button, Flex, Text } from "@mantine/core";
+import { Printer, File } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function PrintButton() {
-    const [supportPrint ,setSupportPrint] = useState(globalThis.print !== undefined)
+    const [supportPrint, setSupportPrint] = useState(globalThis.print !== undefined)
 
-    // useEffect(() => {
-    //     setSupportPrint(globalThis.print !== undefined)
-    // }, [])
+    useEffect(() => {
+        const printWindowOpened = () => {
+            setSupportPrint(false)
+        }
+
+        window.addEventListener("beforeprint", printWindowOpened);
+
+        return () => {
+            window.removeEventListener("beforeprint", printWindowOpened)
+        }
+    }, [])
 
     return <CustomMantineProvider>
         {supportPrint ? (
@@ -19,8 +27,11 @@ export default function PrintButton() {
             </Affix>
         ) : (
             <Affix position={{ bottom: 20, right: 20 }}>
-                <Button variant="round" onClick={() => print()}>
-                    <Download size={26} />
+                <Button variant="round" leftSection={<File size={26} />} onClick={() => location.href="https://res.cloudinary.com/dmq8ipket/image/upload/v1757748988/Resume_-_Jian_Gong_-_Web_Game_Dev_kkddqr.pdf"}>
+                   <Flex direction={"column"}>
+                        <Text size="xs">Not Working?</Text>
+                        <Text size="xs">Download PDF</Text>
+                    </Flex>
                 </Button>
             </Affix>
         )}
